00fd6013edcc70df50ef6d199d4e8a2641957e66
Phie <phie@phie.ovh>
2023-04-19 22:27:08 +0000

    so many things

--- gemfile.php
+++ gemfile.php
@@ -9,8 +9,13 @@
 **/
 require_once(plugin_dir_path(__FILE__) . '/vendor/autoload.php');
 use League\HTMLToMarkdown\HtmlConverter;
-function wporg_custom( $post_id, $post ) {
-    // do something
+
+function getFileName($post){
+ return $post->ID."-".sanitize_file_name($post->post_title).".gmi";
+
+}
+
+function createGmiFromPost($post) {
 $converter = new HtmlConverter();
 $markdown = $converter->convert($post->post_content);
 
@@ -18,13 +23,29 @@ mkdir("gemfiles");
 //ob_start();
 //var_dump($post->post_content);
 //$data = ob_get_clean();
-$finalText="# ".$post->post_title."\n\n".$markdown;
-$fname = $post->ID."-".$post->post_title.".gmi";
+$finalText="# ".$post->post_title."\n".$post->post_date."\n by ".get_author_name($post->post_author)."\n\n".$markdown;
+$fname = getFileName($post);
 $fp = fopen("gemfiles/".$fname, "w");
 fwrite($fp, $finalText);
 fclose($fp);
-$fp = fopen("gemfiles/index.gmi", "a");
-fwrite($fp, "\n=> ".$fname." ".$post->post_title);
+
+}
+
+function wporg_custom( $post_id, $post ) {
+    // do something
+createGmiFromPost($post);
+$index = file_get_contents("gemfiles/index_header.gmi");
+
+$posts = get_posts( array(
+	'numberposts'	=> -1
+));
+foreach ($posts as $p) {
+//createGmiFromPost($p);
+$fname = getFileName($p);
+$index .="\n=> ".$fname." ".$p->post_title." (".$post->post_date.")\n";
+}
+$fp = fopen("gemfiles/index.gmi", "w");
+fwrite($fp, $index);
 fclose($fp);
 
 }
